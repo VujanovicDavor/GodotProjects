4319efcc7ce4479f645aa6b66975fce886ca10d3
VujanovicDavor <vujanovicdavor8@gmail.com>
2022-12-02 00:55:58 +0100

    added physics layers

--- Movement/Player.gd
+++ Movement/Player.gd
@@ -8,10 +8,38 @@ export var gravity = 400
 onready var max_screen_size = get_viewport_rect().size
 
 func _physics_process(delta):
-	velocity.x = get_move_velocity(velocity) * speed
+	velocity.x = get_move_velocity(velocity)
 	velocity.y = get_vertical_velocity(velocity,delta)
+	
 	move_and_slide(velocity, Vector2.UP)
 	
+	#reset position if player is offsreen
+	position = reset_position(position)
+	
+func get_move_velocity(move_velocity: Vector2):
+	move_velocity.x = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) * speed
+		
+	return move_velocity.x
+	
+func get_vertical_velocity(vertical_velocity: Vector2, delta: float):
+	if is_on_floor():
+		vertical_velocity.y = 0
+		jump_counter = 0
+	
+	if jump_counter < MAX_JUMP_COUNTER:
+		if Input.is_action_just_pressed("jump"):
+			jump_counter += 1
+			vertical_velocity.y = -jump_force
+	
+	if !is_on_floor():
+		vertical_velocity.y += gravity  * delta
+	
+	if vertical_velocity.y > MAX_VELOCITY:
+		vertical_velocity.y = MAX_VELOCITY
+	
+	return vertical_velocity.y
+
+func reset_position(position :Vector2):
 	if position.y > max_screen_size.y:
 		position = Vector2(position.x,0)
 		
@@ -20,31 +48,8 @@ func _physics_process(delta):
 		
 	if position.x < 0:
 		position = Vector2(max_screen_size.x - 1,position.y)
-	
-func get_move_velocity(move_velocity: Vector2):
-	move_velocity.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 		
-	return move_velocity.x
-	
-func get_vertical_velocity(vertical_velocity: Vector2, delta: float):
-	print(jump_counter)
-	if is_on_floor():
-		vertical_velocity.y = 0
-		jump_counter = 0
-		#print(jump_counter)
-	
-	if jump_counter <= MAX_JUMP_COUNTER:
-		if Input.is_action_just_pressed("jump"):
-			jump_counter += 1
-			vertical_velocity.y = -jump_force
-	
-	if !is_on_floor():
-		if jump_counter < MAX_JUMP_COUNTER:
-			vertical_velocity.y += gravity  * delta
-		else:
-			vertical_velocity.y += gravity * delta
-	
-	#if vertical_velocity.y > MAX_VELOCITY:
-	#	vertical_velocity.y = MAX_VELOCITY
-	
-	return vertical_velocity.y
+	return position
+
+func wall_jump():
+	pass
